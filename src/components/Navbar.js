8b87c2757ff26feab0a815d3ae539712455cac98
Sontara444 <sontararajput@gmail.com>
2023-10-15 00:12:34 +0530

    navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <div className="logo">
+                <img src="/images/Nike-Logo.png" alt="" />
+            </div>
+            <ul>
+                <li href="#">Menu</li>
+                <li href="#">Location</li>
+                <li href="#">About</li>
+                <li href="#">Contect</li>
+            </ul>
+            <button>Login</button>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
